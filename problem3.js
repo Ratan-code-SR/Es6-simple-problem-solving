92595181bbfc5f4e17eb239bdc79e515e2e02cc6
ratan <ratanchakma68@gmail.com>
2024-02-27 12:23:05 +0600

    problem 3 add

--- problem3.js
+++ problem3.js
@@ -5,5 +5,20 @@
 // c) Return the average of the sum of the squared elements 
 
 // Print the result. 
+const myFunction = (numbers) => {
+    let sum = 0;
+    for (const number of numbers) {
+        const square = number * 2;
+        sum += square;
+    }
 
+    const result = sum / numbers.length;
+    return result;
+
+}
+
+
+const numbers = [10, 20, 30, 40, 50, 60];
+const output = myFunction(numbers)
+console.log(output);
 
